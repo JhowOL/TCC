341cfa9428e1539935d64d66115b947e2a33c852
Jhonatan <jhonatanlemos99@gmail.com>
2023-08-15 19:44:22 -0300

    add claas-validator

--- src/stocks/dto/createProduct.dto.ts
+++ src/stocks/dto/createProduct.dto.ts
@@ -1,6 +1,12 @@
+import { IsNotEmpty, ValidateNested } from "class-validator"
 import { Products } from "./product.dto"
+import { Type } from "class-transformer";
 
-export class CreateProducts{
-    stockId: string
-    products: Products[]
+export class CreateProducts {
+    @IsNotEmpty()
+    stockId: string;
+    @IsNotEmpty()
+    @ValidateNested({ each: true })
+    @Type(() => Products)
+    products: Products[];
 }
--- src/stocks/dto/product.dto.ts
+++ src/stocks/dto/product.dto.ts
@@ -1,12 +1,26 @@
-export class Products{
-        name: string;
-        amount: number;
-        category: string;
-        lastPurchase: LastPurchase;
+import { IsNotEmpty, Min, ValidateNested } from "class-validator";
+import { Type } from "class-transformer";
+
+export class LastPurchase {
+    @IsNotEmpty()
+    @Min(1)
+    amount: number;
+    @IsNotEmpty()
+    @Min(0.01)
+    unitaryPrice: number;
+    @IsNotEmpty()
+    date: Date;
 }
 
-export class LastPurchase{
+export class Products {
+    @IsNotEmpty()
+    name: string;
+    @IsNotEmpty()
     amount: number;
-    unitaryPrice: number;
-    date: Date;
+    @IsNotEmpty()
+    category: string;
+    @IsNotEmpty()
+    @ValidateNested({ each: true })
+    @Type(() => LastPurchase)
+    lastPurchase: LastPurchase;
 }
